434ef8b50f5c841275972927278e9cbc1be3014b
Askhab Magomedov <asmag86@gmail.com>
2021-05-02 00:40:15 +0300

    Smal changes in html

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-let ballWidth = $('.ball').innerWidth();
-  console.log(`Ball width - ${ballWidth}`);
-let ballHeight = $('.ball').innerHeight();
-  console.log(`Ball height - ${ballHeight}`);
 
-let fieldWidth = $('.field').innerWidth();
-  console.log(`Field width - ${fieldWidth}`);
-let fieldHeight = $('.field').innerHeight();
-  console.log(`Field height - ${fieldHeight}`);
 
-$('.ball').on('click', function(event) {
-  let lineX = event.pageX;
-  let lineY = event.pageY;
-  
-  $(this).animate({left:'+=250px'}, 'fast');
-  $(this).animate({top:'+=50px'}, 'slow');
-  
-  let text = `Field width = ${fieldWidth} - Field height = ${fieldHeight}
-  \nBall position-X = ${lineX} - Ball position-Y = ${lineY}`;
-  
-  $('.coords').text(text);
+// let ballWidth = $('.ball').innerWidth();
+//   console.log(`Ball width - ${ballWidth}`);
+// let ballHeight = $('.ball').innerHeight();
+//   console.log(`Ball height - ${ballHeight}`);
 
-  if(lineX < fieldWidth || lineX > fieldWidth) {
-    console.info(`Ball out of the field`);
-  }
-});
+// let fieldWidth = $('.field').innerWidth();
+//   console.log(`Field width - ${fieldWidth}`);
+// let fieldHeight = $('.field').innerHeight();
+//   console.log(`Field height - ${fieldHeight}`);
+
+// $('.ball').on('click', function(event) {
+//   let lineX = event.pageX;
+//   let lineY = event.pageY;
+  
+//   $(this).animate({left:'+=250px'}, 'fast');
+//   $(this).animate({top:'+=50px'}, 'slow');
+  
+//   let text = `Field width = ${fieldWidth} - Field height = ${fieldHeight}
+//   \nBall position-X = ${lineX} - Ball position-Y = ${lineY}`;
+  
+//   $('.coords').text(text);
+
+//   if(lineX < fieldWidth || lineX > fieldWidth) {
+//     console.info(`Ball out of the field`);
+//   }
+// });
