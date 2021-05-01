6f7c62d9d7183bd1fd19eb95396df3751ad0109d
Askhab Magomedov <asmag86@gmail.com>
2021-05-02 01:12:12 +0300

    Added animation for ball

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
-
-
-// let ballWidth = $('.ball').innerWidth();
-//   console.log(`Ball width - ${ballWidth}`);
-// let ballHeight = $('.ball').innerHeight();
-//   console.log(`Ball height - ${ballHeight}`);
-
-// let fieldWidth = $('.field').innerWidth();
-//   console.log(`Field width - ${fieldWidth}`);
-// let fieldHeight = $('.field').innerHeight();
-//   console.log(`Field height - ${fieldHeight}`);
-
-// $('.ball').on('click', function(event) {
-//   let lineX = event.pageX;
-//   let lineY = event.pageY;
-  
-//   $(this).animate({left:'+=250px'}, 'fast');
-//   $(this).animate({top:'+=50px'}, 'slow');
-  
-//   let text = `Field width = ${fieldWidth} - Field height = ${fieldHeight}
-//   \nBall position-X = ${lineX} - Ball position-Y = ${lineY}`;
-  
-//   $('.coords').text(text);
-
-//   if(lineX < fieldWidth || lineX > fieldWidth) {
-//     console.info(`Ball out of the field`);
-//   }
-// });
+let ball = $('.ball'),
+    ballWidth = ball.width(),
+    ballHeight = ball.height(),
+    field = $('.field'),
+    fieldWidth = field.width(),
+    fieldHeight = field.height();
+    
+$(ball).on('click', function() {
+    let randomHeight = Math.floor(Math.random() * (fieldHeight + 1));
+    console.log(randomHeight);
+    $(this).animate({left: fieldWidth - ballWidth, top: randomHeight}, '400');
+});
