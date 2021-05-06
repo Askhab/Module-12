75d6bb42c1c9e1478012deb92402daedd3fec51e
Askhab <asmag86@gmail.com>
2021-05-06 16:01:16 +0300

    Added new function for actions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 let ball = $('.ball'),
     ballWidth = ball.width(),
     ballHeight = ball.height(),
+    ballLineX = ball.offset().left,
+    ballLineY = ball.offset().top,
     field = $('.field'),
     fieldWidth = field.width(),
     fieldHeight = field.height(),
-    goal = $('.goal');
+    goalLeft = $('.leftSide'),
+    goalRight = $('.rightSide');
 
-console.info(goal.position());
-    
+// console.info(ballLineX);
+// console.info(ballLineY);
+console.info(goalLeft.position().top);
+console.info(goalLeft.position().left);
+console.info(goalRight.position().top);
+console.info(goalRight.position().left);
+
 $(ball).on('click', function() {
   let ballPosition = ball.position(),
-      randomHeight = getRandomNumber(fieldHeight);
+  goalLeftPosition = goalLeft.position(),
+  goalRightPosition = goalRight.position(),
+  randomHeight = getRandomNumber(fieldHeight);
 
   if(ballPosition.left === 0) {
     $(this).animate({left: fieldWidth - ballWidth, top: randomHeight}, '400');
   } else if(ballPosition.left === fieldWidth - ballWidth) {
     $(this).animate({left: fieldWidth - fieldWidth, top: randomHeight}, '400');
+  }
+
+  if(
+    (ballPosition.left - ballWidth > goalLeftPosition.left && ballPosition.right - ballHeight > goalLeftPosition.top) ||
+    (ballPosition.left - ballWidth > goalRightPosition.left && ballPosition.top - ballHeight > goalRightPosition.top))
+  {
+    alert(`Гооол!`);
   }
 });
 
